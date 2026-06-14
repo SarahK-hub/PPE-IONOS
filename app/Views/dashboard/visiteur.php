e1c8123daf88e6fce953b68e03dbbb9636268bae
SarahK-hub <taieb.sarah2004@gmail.com>
2026-06-14 22:30:00 +0200

    Update visiteur.php

--- app/Views/dashboard/visiteur.php
+++ app/Views/dashboard/visiteur.php
@@ -2,11 +2,11 @@
 <h1>Dashboard Visiteur</h1>
 
 <div class="cards">
-    <a class="card" href="/fichefrais/create">
+    <a class="card" href="/index.php/fichefrais/create">
          Saisir fiche de frais
     </a>
 
-    <a class="card" href="/mes-fiches">
+    <a class="card" href="/index.php/mes-fiches">
         Consulter mes fiches
     </a>
 </div>
