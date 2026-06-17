baba04042bf4559dcb5e7f03911f9d070f4c839a
SarahK-hub <taieb.sarah2004@gmail.com>
2026-06-17 10:22:30 +0200

    Update comptable.php

--- app/Views/dashboard/comptable.php
+++ app/Views/dashboard/comptable.php
@@ -1,10 +1,25 @@
-<div class="container">
-<h1>Dashboard Comptable</h1>
+<h1 class="dashboard-title">Tableau de bord - Comptable</h1>
 
-<div class="cards">
-    <a class="card" href="/index.php/fichefrais">
-         Suivi des fiches
+<p class="welcome">
+    Bienvenue <?= htmlspecialchars($_SESSION['user']['login']) ?>
+</p>
+
+<div class="dashboard-grid">
+
+    <a href="index.php/fichefrais" class="card">
+        <h2> Suivi des fiches</h2>
+        <p>Valider et suivre les fiches de frais</p>
     </a>
-</div>
+
+    <a href="/index.php/visiteur" class="card">
+        <h2> Gérer les visiteurs</h2>
+        <p>Ajouter, modifier ou supprimer</p>
+    </a>
+
+ <a href="/index.php/etat" class="card">
+        <h2> Gérer les etats</h2>
+        <p>Ajouter, modifier ou supprimer</p>
+    </a>
+
 
 </div>
